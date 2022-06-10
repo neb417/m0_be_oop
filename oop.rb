9a2a881d12f7d9b4515e4ebaf53342c87136bddc
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-10 12:47:46 -0600

    Commit beginning hobbit code

--- oop.rb
+++ oop.rb
@@ -69,16 +69,15 @@ p vamp1
           @is_hungry = false
         end
       end
-
   end
+
 
 drag1 = Dragon.new("Lizzy", "Ranger", "Magenta")
 
 p drag1
 
-drag1.satiated(1)
+drag1.satiated(4)
 p drag1
-
 
  # Write a Hobbit class
  # it should have a dynamic name attribute (string)
@@ -88,3 +87,25 @@ p drag1
  # it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
  # it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
  # it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+  def initialize(name, disposition)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult = false
+    @is_old = false
+    has_ring = false
+  end
+
+  def celebrate_birthday
+    @age.next
+  end
+
+end
+
+hob1 = Hobbit.new("Sam", "loopy")
+p hob1
+
+hob1.celebrate_birthday
+p hob1
