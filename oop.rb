1150d34b9b7a5d734ea15c1a9e2e31ebf4bb1c09
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-10 11:39:47 -0600

    Commit changes to add dragon oop proj

--- oop.rb
+++ oop.rb
@@ -26,6 +26,29 @@ uni1.say
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    def initialize(name)
+      @name = name
+      @pet = "bat"
+      @thirsty = true
+    end
+
+    def change_pet(new_pet)
+      @pet = new_pet
+    end
+
+    def drink
+      @thirsty = false
+    end
+end
+
+vamp1 = Vampire.new("Bram")
+
+vamp1.change_pet("kitty cat")
+vamp1.drink
+
+p vamp1
+
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -33,13 +56,35 @@ uni1.say
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+ class Dragon
+      def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+      end
+
+      def satiated(has_eaten)
+        if has_eaten >= 4
+          @is_hungry = false
+        end
+      end
+
+  end
+
+drag1 = Dragon.new("Lizzy", "Ranger", "Magenta")
+
+p drag1
+
+drag1.satiated(1)
+p drag1
 
 
-#  Write a Hobbit class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic disposition attribute (string)
-#  it should have an age attribute that defaults to 0
-#  it should have a celebrate_birthday method. When called, the age increases by 1
-#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
-#  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
-#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+ # Write a Hobbit class
+ # it should have a dynamic name attribute (string)
+ # it should have a dynamic disposition attribute (string)
+ # it should have an age attribute that defaults to 0
+ # it should have a celebrate_birthday method. When called, the age increases by 1
+ # it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
+ # it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
+ # it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
